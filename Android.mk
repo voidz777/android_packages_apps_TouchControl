1d7d9662ae9e9d762c673a7485a2578ae65b630d
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-12-08 05:21:01 +0000

    dont build as privileged app and some layout fixes

--- Android.mk
+++ Android.mk
@@ -19,7 +19,6 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := TouchControl
 
 include $(BUILD_PACKAGE)
